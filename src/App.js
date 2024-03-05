e1b0ac861dbd248e19184fd9eb39530f0a42c66d
buseseker <sekerbuse_96@hotmail.com>
2024-03-06 01:16:24 +0300

    Components has been imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React from 'react';
+import SearchIP from './components/search-bar/SearchIP';
+import LocationMap from './components/search-bar/location-map/LocationMap';
+import IPProvider from './context/ip-context/IPContext';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <IPProvider>
+      <SearchIP />
+      <LocationMap/>
+    </IPProvider>
   );
 }
 
