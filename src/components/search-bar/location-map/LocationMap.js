3adcded8588f6ad4a4a90bc020061080e08e75f4
buseseker <sekerbuse_96@hotmail.com>
2024-03-06 03:25:23 +0300

    Code refactoring

--- src/components/search-bar/location-map/LocationMap.js
+++ src/components/search-bar/location-map/LocationMap.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
+import { IPContext } from '../../../context/ip-context/IPContext';
 
 const LocationMap = () => {
-  const position = [51.505, -0.09];
+  const { data } = useContext(IPContext);
+
+  let position = [[51.505, -0.09]];
+
+  if (data && data.location) {
+    position = [data.location.lat, data.location.lng];
+  } else {
+    position = [[51.505, -0.09]];
+  }
+
+  console.log(position);
 
   return (
-    <div className="location-map"> {/* Haritaya stil sınıfını uygulayın */}
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <div className='location-map'>
+      <MapContainer key={`${position[0]}-${position[1]}`} center={position} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
         <Marker position={position}>
           <Popup>
--- src/context/ip-context/IPContext.js
+++ src/context/ip-context/IPContext.js
@@ -1,5 +1,7 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
+import SearchIP from '../../components/search-bar/SearchIP';
+import LocationMap from '../../components/search-bar/location-map/LocationMap';
 
 export const IPContext = createContext();
 
@@ -7,18 +9,52 @@ const IPProvider = ({ children }) => {
   const [ip, setIp] = useState('');
   const [data, setData] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        `https://geo.ipify.org/api/v2/country?apiKey=at_I6dPAQagAdidJp0hosUB1e00rLhYL&ipAddress=${ip}`
-      );
-      setData(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+  useEffect(() => {
+    const fetchCurrentIP = async () => {
+      try {
+        const response = await axios.get(`https://api.ipify.org/?format=json`);
+        setIp(response.data.ip);
+      } catch (error) {
+        console.error('Error fetching IP:', error);
+      }
+    };
+    fetchCurrentIP();
+  }, []);
 
-  return <IPContext.Provider value={{ ip, setIp, data, fetchData }}>{children}</IPContext.Provider>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=at_I6dPAQagAdidJp0hosUB1e00rLhYL&ipAddress=${ip}`
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (ip) {
+      fetchData();
+    }
+  }, [ip]);
+
+  // check if data is exist
+  if (!data) {
+    return (
+      <IPContext.Provider value={{ ip, setIp, data }}>
+        {children}
+        <SearchIP />
+      </IPContext.Provider>
+    );
+  }
+
+  return (
+    <IPContext.Provider value={{ ip, setIp, data }}>
+      {children}
+      <SearchIP />
+      <LocationMap />
+    </IPContext.Provider>
+  );
 };
 
 export default IPProvider;
