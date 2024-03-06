b8c6ac21d1df8bf803f3ab9313864f3ccce56933
buseseker <sekerbuse_96@hotmail.com>
2024-03-06 13:44:47 +0300

    Initial state of map loading has been added

---
+++ src/components/location-map/LocationMap.js
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { IPContext } from '../../context/ip-context/IPContext';
+import { Icon } from 'leaflet';
+
+const LocationMap = () => {
+  const { data } = useContext(IPContext);
+  const [loaded, setLoaded] = useState(false); // Initial state of map loading
+
+  let position = [[51.505, -0.09]];
+
+  if (data && data.location) {
+    position = [data.location.lat, data.location.lng];
+  } else {
+    position = [[51.505, -0.09]];
+  }
+
+  console.log(position);
+
+  const markerIcon = new Icon({
+    iconUrl: require('../../assets/images/marker-icon.png'),
+    iconSize: [50, 50],
+  });
+
+  useEffect(() => {
+    if (data) {
+      setLoaded(true); // Update state of map loading true when data is received
+    }
+  }, [data]);
+
+  return (
+    <div className={`location-map ${loaded ? 'loaded' : ''}`}>
+      <MapContainer key={`${position[0]}-${position[1]}`} center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position} icon={markerIcon}></Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default LocationMap;
