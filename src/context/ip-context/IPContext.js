5f4d8ce65ed162b570ac15a97976fcdf598ee294
buseseker <sekerbuse_96@hotmail.com>
2024-03-06 12:50:23 +0300

    fetchData has been defined outside useeffect

--- src/context/ip-context/IPContext.js
+++ src/context/ip-context/IPContext.js
@@ -21,38 +21,29 @@ const IPProvider = ({ children }) => {
     fetchCurrentIP();
   }, []);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://geo.ipify.org/api/v2/country,city?apiKey=at_I6dPAQagAdidJp0hosUB1e00rLhYL&ipAddress=${ip}`
-        );
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_I6dPAQagAdidJp0hosUB1e00rLhYL&ipAddress=${ip}`
+      );
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
+  useEffect(() => {
     if (ip) {
       fetchData();
     }
   }, [ip]);
 
-  // check if data is exist
-  if (!data) {
-    return (
-      <IPContext.Provider value={{ ip, setIp, data }}>
-        {children}
-        <SearchIP />
-      </IPContext.Provider>
-    );
-  }
-
+  // Return IPContext.Provider with appropriate values
   return (
-    <IPContext.Provider value={{ ip, setIp, data }}>
+    <IPContext.Provider value={{ ip, setIp, data, fetchData }}>
       {children}
       <SearchIP />
-      <LocationMap />
+      {data && <LocationMap />}
     </IPContext.Provider>
   );
 };
