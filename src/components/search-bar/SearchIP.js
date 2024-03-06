d97761754ac540422d91a86dacdb5143f2775cc0
buseseker <sekerbuse_96@hotmail.com>
2024-03-06 15:27:04 +0300

    Loader spinner has been added

--- src/components/search-bar/SearchIP.js
+++ src/components/search-bar/SearchIP.js
@@ -3,6 +3,7 @@ import { IPContext } from '../../context/ip-context/IPContext';
 
 const SearchIP = () => {
   const { ip, setIp, data, fetchData } = useContext(IPContext);
+
   return (
     <div className='searchIP'>
       <div className='search-section'>
@@ -27,22 +28,22 @@ const SearchIP = () => {
       <div className='result-section'>
         <div className='ip-address'>
           <h5>IP ADDRESS</h5>
-          <p>{data ? data.ip : ''}</p>
+          <p className={`${!data ? 'loader' : ''}`}>{data ? data.ip : ''}</p>
         </div>
         <div className='seperator'></div>
         <div className='location'>
           <h5>LOCATION</h5>
-          <p>{data ? `${data.location.region}, ${data.location.country}` : ''}</p>
+          <p className={`${!data ? 'loader' : ''}`}>{data ? `${data.location.region}, ${data.location.country}` : ''}</p>
         </div>
         <div className='seperator'></div>
         <div className='timezone'>
           <h5>TIMEZONE</h5>
-          <p>{data ? data.location.timezone : ''}</p>
+          <p className={`${!data ? 'loader' : ''}`}>{data ? data.location.timezone : ''}</p>
         </div>
         <div className='seperator'></div>
         <div className='isp'>
           <h5>ISP</h5>
-          <p>{data ? data.isp : ''}</p>
+          <p className={`${!data ? 'loader' : ''}`}>{data ? data.isp : ''}</p>
         </div>
       </div>
     </div>
