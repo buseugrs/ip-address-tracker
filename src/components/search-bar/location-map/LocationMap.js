5685d7fa6ad86e994ae5892014a0abf8ae87b7bd
buseseker <sekerbuse_96@hotmail.com>
2024-03-06 12:19:18 +0300

    Marker icon has been added

--- src/components/search-bar/location-map/LocationMap.js
+++ src/components/search-bar/location-map/LocationMap.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { IPContext } from '../../../context/ip-context/IPContext';
+import { Icon } from 'leaflet';
 
 const LocationMap = () => {
   const { data } = useContext(IPContext);
@@ -16,6 +17,11 @@ const LocationMap = () => {
 
   console.log(position);
 
+  const markerIcon = new Icon ({
+    iconUrl: require ('../../../assets/images/marker-icon.png'),
+    iconSize: [50,50]
+  })
+
   return (
     <div className='location-map'>
       <MapContainer key={`${position[0]}-${position[1]}`} center={position} zoom={13} scrollWheelZoom={false}>
@@ -23,11 +29,7 @@ const LocationMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <Marker position={position} icon={markerIcon}></Marker>
       </MapContainer>
     </div>
   );
