09aaaeb8332b7fc9dfbf619bab727e91bbaa1190
buseseker <sekerbuse_96@hotmail.com>
2024-03-06 13:44:11 +0300

    LocationMap component path has been changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchIP from './components/search-bar/SearchIP';
-import LocationMap from './components/search-bar/location-map/LocationMap';
+import LocationMap from './components/location-map/LocationMap';
 import IPProvider from './context/ip-context/IPContext';
 
 import './App.scss';
